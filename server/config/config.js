8af403b0590c49f7f4fccef5988796b7c2d56d75
Andres Higueros <higueros71@gmail.com>
2020-11-27 15:52:53 -0600

    add: agregando archivo de configuracion

---
+++ server/config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+module.exports = {
+    user: process.env.USER,
+    password: process.env.PASS,
+    port: process.env.PORT,
+    db: process.env.DB,
+    dbOptions: {
+        host: 'localhost',
+        dialect: 'mysql',
+        pool: {
+            max: 20,
+            min: 0,
+            idle: 10000
+        },
+        logging: false
+    },
+    status: process.env.STATE,
+};
