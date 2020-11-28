78daef5941b583a79811c4bfa0ec0964af14cf05
Andres Higueros <higueros71@gmail.com>
2020-11-28 01:01:08 -0600

    add: agregando funciones para poder encryptar y comparar encripciones

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 function normalizePort(val) {
     var port = parseInt(val, 10);
 
@@ -12,6 +14,26 @@ function normalizePort(val) {
     return false;
 }
 
+function encrypt(text) {
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(text, salt);
+        return hash
+    } catch (error) {
+        return null
+    }
+}
+
+function compareEncrypt(text, hash) {
+    try {
+        return bcrypt.compareSync(text, hash)
+    } catch (error) {
+        return false
+    }
+}
+
 module.exports = {
-    normalizePort
+    normalizePort,
+    encrypt,
+    compareEncrypt
 }
